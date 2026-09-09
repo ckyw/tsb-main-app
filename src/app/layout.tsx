a9b43dadce00cf28633163ea54ce44b5d41da0d5
ckyw <ck.park.marketing@gmail.com>
2026-09-09 14:36:36 +0900

    Apply Pretendard font globally

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata, Viewport } from "next";
+import "pretendard/dist/web/variable/pretendardvariable-dynamic-subset.css";
 import "./globals.css";
 import { siteConfig } from "@/config/site";
 
